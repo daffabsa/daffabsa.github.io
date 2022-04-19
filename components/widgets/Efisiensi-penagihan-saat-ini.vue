ddc1b66b6b0761c9cc207333355d832e381c3ad4
alfydape <daffareri6@gmail.com>
2022-04-19 10:51:37 +0800

    PDAM-5072 #complete efficiency widget

---
+++ components/widgets/Efisiensi-penagihan-saat-ini.vue
@@ -0,0 +1,38 @@
+<script>
+/**
+ * Top-performing component
+ */
+export default {
+  name: "Efisiensi Penagihan Saat ini",
+  data() {
+    return {
+      efficiencyData: {
+        efficiency_billing: 85.88,
+      },
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-body">
+      <div class="badges">
+        <span>BPPSPAM</span>
+      </div>
+
+      <div class="mt-2">
+        <div class="row">
+          <h3 class="heading-text">Efisiensi Penagihan Saat Ini</h3>
+        </div>
+        <div class="row" style="margin-top: 10px">
+          <div class="box-card">
+            <h2>{{ efficiencyData.efficiency_billing }}%</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end card-body -->
+  </div>
+  <!-- end card-->
+</template>
---
+++ components/widgets/Efisiensi-penagihan.vue
@@ -0,0 +1,38 @@
+<script>
+/**
+ * Top-performing component
+ */
+export default {
+  name: "Efisiensi Penagihan",
+  data() {
+    return {
+      efficiencyData: {
+        efficiency_billing: 79.79,
+      },
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-body">
+      <div class="badges">
+        <span>BPPSPAM</span>
+      </div>
+
+      <div class="mt-2">
+        <div class="row">
+          <h3 class="heading-text">Efisiensi Penagihan</h3>
+        </div>
+        <div class="row" style="margin-top: 10px">
+          <div class="box-card">
+            <h2>{{ efficiencyData.efficiency_billing }}%</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end card-body -->
+  </div>
+  <!-- end card-->
+</template>
